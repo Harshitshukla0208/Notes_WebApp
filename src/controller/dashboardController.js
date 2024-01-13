4449c5be91f678b4ebe000171e1a827d08578872
Harshit Shukla <mrharshit2409@gmail.com>
2024-01-13 16:05:00 +0530

    Added Notes Schema

--- src/controller/dashboardController.js
+++ src/controller/dashboardController.js
@@ -1,10 +1,10 @@
 exports.dashboard = async(req, res) => {
-    const locals = {
-        title: "Dashboard",
-        description: "Free NodeJS Notes App"
-    };
-    res.render('dashboard/index', {
-        locals,
-        layout: '../views/layouts/dashboard'
-    })
+  const locals = {
+      title: "Dashboard",
+      description: "Free NodeJS Notes App"
+  };
+  res.render('dashboard/index', {
+      locals,
+      layout: '../views/layouts/dashboard'
+  })
 }
